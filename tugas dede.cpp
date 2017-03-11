3e44de7761fb2caf90d8f58f1ff6f51e6b898287
dede yayat <dedeyayat00@gmail.com>
2017-03-11 20:25:20 +0700

    TUGAS PROJECT C++

---
+++ tugas dede.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "hello world" << endl;
+cout << "i love living in this world" << endl;
+cout << " nama  : Dede Yayat" << endl;
+cout << " kelas : TIF 16B MALAM" << endl;
+cout << " nim   : 16 111 183" << endl;
+return 0;
+} 
